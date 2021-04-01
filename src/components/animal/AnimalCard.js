1ac1733c8ab44da93ca41c3ee7f8c601438ec07b
MegMcGregor <megmcgregor1@gmail.com>
2021-04-01 15:55:36 -0500

    initial setup

---
+++ src/components/animal/AnimalCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Animal.css";
+
+export const AnimalCard = () => (
+    <section className="animal">
+        <h3 className="animal__name">Poppy</h3>
+        <div className="animal__breed">Breed: Beagle Mix</div>
+    </section>
+)
