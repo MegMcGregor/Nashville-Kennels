614d617ea3328629ab6792e64ab71fd87d243f0c
MegMcGregor <megmcgregor1@gmail.com>
2021-04-07 14:04:20 -0500

    moved discharge button to animal details

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { findRenderedComponentWithType } from "react-dom/test-utils";
-import { getAnimalById } from "../../modules/AnimalManager";
+import { deleteAnimal, getAnimalById } from "../../modules/AnimalManager";
 import "./AnimalDetail.css";
 import { useParams, useHistory } from "react-router-dom";
 
+
 export const AnimalDetail = () => {
     const [animal, setAnimal] = useState({ name: "", breed: "" });
+    const [isLoading, setIsLoading] = useState(true);
 
     const { animalId } = useParams();
     const history = useHistory();
@@ -18,8 +20,16 @@ export const AnimalDetail = () => {
                     name: animal.name,
                     breed: animal.breed
                 });
+                setIsLoading(false);
             });
     }, [animalId])
+
+    const handleDelete = () => {
+        setIsLoading(true);
+        deleteAnimal(animalId).then(() => 
+        history.push("/animals") 
+        );
+    };
 
     return (
         <section className="animal">
@@ -27,6 +37,9 @@ export const AnimalDetail = () => {
             <div className="animal__breed">{animal.breed}</div>
             <div className="animal__location">Location: {animal.location?.name}</div>
             <div className="animal__owner">Customer: {animal.customer?.name}</div>
+            <button type="button" disabled={isLoading} onClick={handleDelete}>
+                Discharge
+        </button>
         </section>
-    )
-}
+    );
+};
