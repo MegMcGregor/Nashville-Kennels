f2e287095e0cc6bab0b497ffee054520e54da671
MegMcGregor <megmcgregor1@gmail.com>
2021-04-09 16:04:21 -0500

    login on animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Home } from './Home';
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { LocationList } from "./location/LocationList"
 import { AnimalList } from "./animal/AnimalList"
 import { CustomerList } from "./customer/CustomerList";
 import { EmployeeList } from "./employee/EmployeeList";
 import { AnimalDetail } from "./animal/AnimalDetail";
 import { AnimalForm } from "./animal/AnimalForm"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const ApplicationViews = () => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("kennel_customer") !== null)
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("kennel_customer", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+    }
+
     return (
         <>
             <Route exact path="/">
@@ -20,7 +30,15 @@ export const ApplicationViews = () => {
             </Route>
 
             <Route exact path="/animals">
-                <AnimalList />
+                {isAuthenticated ? <AnimalList /> : <Redirect to="/login" />}
+            </Route>
+
+            <Route path="/login">
+                <Login setAuthUser={setAuthUser}/>
+            </Route>
+
+            <Route path="/register">
+                <Register setAuthUser={setAuthUser}/>
             </Route>
 
             <Route path="/animals/:animalId(\d+)">
